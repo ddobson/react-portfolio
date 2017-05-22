5aeb20e442df23a8f1091992ed9ebf1899600b62
ddobson <ddobson445@gmail.com>
2017-05-22 16:07:57 -0400

    Create desktop menu
    - Add markup for the desktop menu

---
+++ src/components/DesktopMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const DesktopMenu = () => {
+  return (
+    <div className="desktop-menu">
+      <ul>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default DesktopMenu;
