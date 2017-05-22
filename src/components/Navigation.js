e9bc09ebdbe565c0523965d5209448505ae84324
ddobson <ddobson445@gmail.com>
2017-05-22 16:52:31 -0400

    Transition navbar color on scroll
    - Navbar is fixed and transitions color on scroll

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,29 +8,40 @@ import '../styles/Navigation.scss';
 class Navigation extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      windowWidth: window.innerWidth
+      windowWidth: window.innerWidth,
+      showNavBg: false
     };
 
     this.updateWidowWidth = this.updateWidowWidth.bind(this);
+    this.shouldShowNavBg = this.shouldShowNavBg.bind(this);
   }
 
   updateWidowWidth() {
     this.setState({ windowWidth: window.innerWidth });
   }
 
+  shouldShowNavBg() {
+    const showNavBg = window.scrollY > 60 ? true : false;
+    this.setState({ showNavBg });
+  }
+
   componentDidMount() {
     window.addEventListener('resize', this.updateWidowWidth);
+    window.addEventListener('scroll', this.shouldShowNavBg);
   }
 
   render() {
     const menu = this.state.windowWidth <= 768 ? <BurgerMenu/> : <DesktopMenu/>;
 
     return (
-      <nav className="nav-bar">
-        <h1>Dale Dobson</h1>
-        { menu }
-      </nav>
+      <div className="nav-wrapper">
+        <nav className={this.state.showNavBg ? 'nav-bar nav-bg' : 'nav-bar'}>
+          <h1>Dale Dobson</h1>
+          { menu }
+        </nav>
+      </div>
     );
   }
 }
