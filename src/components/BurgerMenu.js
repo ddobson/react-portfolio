82cf99307db7e8f775aa8bf722be1b610de5b2ec
ddobson <ddobson445@gmail.com>
2017-05-22 16:07:11 -0400

    Create a burger menu component
    - use react burger menu for response navigation

---
+++ src/components/BurgerMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+
+const BurgerMenu = () => {
+  return (
+    <Menu right>
+      <a id="About" href="#about">About</a>
+      <a id="Skills" href="#skills">Skills</a>
+      <a id="Projects" href="#projects">Projects</a>
+      <a id="Contact" href="#contact">Contact</a>
+    </Menu>
+  );
+};
+
+export default BurgerMenu;
