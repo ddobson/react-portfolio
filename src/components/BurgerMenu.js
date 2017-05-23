8285d2a30a08e3e44c7c7f126c9a852d46a0d75e
ddobson <ddobson445@gmail.com>
2017-05-23 11:45:24 -0400

    Fix issue where burger menu placement
    - Burger menu was not aligned with the top of the page
    - Fix flex styles to account for this

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -3,12 +3,14 @@ import { slide as Menu } from 'react-burger-menu';
 
 const BurgerMenu = () => {
   return (
-    <Menu right>
-      <a id="About" href="#about">About</a>
-      <a id="Skills" href="#skills">Skills</a>
-      <a id="Projects" href="#projects">Projects</a>
-      <a id="Contact" href="#contact">Contact</a>
-    </Menu>
+    <div className="burger-wrap">
+      <Menu right>
+        <a id="About" href="#about">About</a>
+        <a id="Skills" href="#skills">Skills</a>
+        <a id="Projects" href="#projects">Projects</a>
+        <a id="Contact" href="#contact">Contact</a>
+      </Menu>
+    </div>
   );
 };
 
