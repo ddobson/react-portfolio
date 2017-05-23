7a06ce9b675d3b2b93c982d51c542e2794840a0d
ddobson <ddobson445@gmail.com>
2017-05-23 17:34:05 -0400

    Create Intro Component
    - Add intro content component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,6 +5,11 @@ const Intro = () => {
     <div className="intro-content">
       <h1>Dale Dobson</h1>
       <hr/>
+      <span>Web Developer</span>
+      <div className="intro-links">
+        <a href="https://github.com/ddobson"><i className="fa fa-2x fa-github"></i></a>
+        <a href="https://linkedin.com/in/dobsondale"><i className="fa fa-2x fa-linkedin"></i></a>
+      </div>
     </div>
   );
 };
