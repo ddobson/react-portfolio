ba50a9f6ed61fb56b91b9f76cd6511ba1b80e2b9
ddobson <ddobson445@gmail.com>
2017-05-23 11:48:37 -0400

    Header Content and Style
    - Add background image to header
    - Add name to intro content

---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div className="intro-content">
+      <h1>Dale Dobson</h1>
+      <hr/>
+    </div>
+  );
+};
+
+export default Intro;
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Intro from '../components/Intro';
+
+import '../styles/Header.scss';
 
 const Header = () => {
   return (
     <div className="header">
       <Navigation/>
+      <Intro/>
     </div>
   );
 };
