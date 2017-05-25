687360a5bdf36417dce37585a1361278b28f3293
ddobson <ddobson445@gmail.com>
2017-05-25 12:24:11 -0400

    Create a data file to keep content organized
    - Create a file with data needed for page content

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = {
+  profileText1: 'I\'m a web developer based in Southern New England. Software development combines my passion for technology with creative freedom and constant personal growth.',
+  profileText2: 'As a former Quality Engineer at Teespring, I understand what it means to deliver a product and support it in production. As I move into a full-time development role my goal is to take that experience to the next level, building awesome applications that put quality first.',
+  skills: [
+    [
+      { name: 'JavaScript', cssClassName: 'javascript' },
+      { name: 'React', cssClassName: 'reactjs' },
+      { name: 'Ruby', cssClassName: 'ruby' },
+      { name: 'Node', cssClassName: 'nodejs' }
+    ],
+    [
+      { name: 'Rails', cssClassName: 'ruby-on-rails' },
+      { name: 'Postgres', cssClassName: 'postgres' },
+      { name: 'MongoDB', cssClassName: 'mongodb' },
+      { name: 'Git', cssClassName: 'git' }
+    ],
+    [
+      { name: 'HTML5', cssClassName: 'html5' },
+      { name: 'CSS3', cssClassName: 'css3' },
+      { name: 'Sass', cssClassName: 'sass' },
+      { name: 'jQuery', cssClassName: 'jquery' }
+    ]
+  ]
+};
+
+export default data;
