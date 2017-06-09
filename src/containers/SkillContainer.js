7942b09cc5611a2befdb93dc221d36fa99bd6f2e
ddobson <ddobson445@gmail.com>
2017-06-09 14:37:59 -0400

    Fade in skills on scroll
    - Wrap container in animation group
    - Fade in on scroll
    - Set min-height of container to avoid jankyness

--- src/containers/SkillContainer.js
+++ src/containers/SkillContainer.js
@@ -1,37 +1,69 @@
 import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 import Skill from '../components/Skill';
 
 import data from '../data';
 
-const SkillContainer = () => {
-  const content = data.skills.map((skillRow, i) => {
-    return (
-      <Row center="xs" key={`skillRow-${i}`}>
-        { skillRow.map((skill, k) => <Skill key={`skill-${k}`} skill={skill}/>) }
-      </Row>
-    );
-  });
+class SkillContainer extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="skill-container">
-      <Row>
-        <Col xs={12}>
-          <div className="skill-intro">
-            <h1>Skills</h1>
-            <p>My skills are focused on JavaScript &amp Ruby on Rails.</p>
-            <hr/>
-          </div>
-        </Col>
-      </Row>
-      <Row className="icon-row-wrap">
-        <Col xs={12}>
-          { content }
-        </Col>
-      </Row>
-    </div>
-  );
-};
+    this.state = {
+      showContainerItems: false
+    };
+
+    this.shouldRenderContainerItems = this.shouldRenderContainerItems.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.shouldRenderContainerItems);
+  }
+
+  shouldRenderContainerItems() {
+    const containerTop = this.refs.skillContainer.getBoundingClientRect().top - window.innerHeight;
+    const showContainerItems = containerTop <= -50 ? true : false;
+
+    this.setState({ showContainerItems });
+  }
+
+  renderSkills() {
+    return data.skills.map((skillRow, i) => {
+      return (
+        <Row center="xs" key={`skillRow-${i}`}>
+          { skillRow.map((skill, k) => <Skill key={`skill-${k}`} skill={skill}/>) }
+        </Row>
+      );
+    });
+  }
+
+  render() {
+    const animationContainer = (
+      <CSSTransitionGroup transitionName="fade" transitionAppear={true} transitionAppearTimeout={1500} transitionEnter={false} transitionLeave={false}>
+        <Row>
+          <Col xs={12}>
+            <div className="skill-intro">
+              <h1>Skills</h1>
+              <p>My skills are focused on JavaScript &amp; Ruby on Rails.</p>
+              <hr/>
+            </div>
+          </Col>
+        </Row>
+        <Row className="icon-row-wrap">
+          <Col xs={12}>
+            { this.renderSkills() }
+          </Col>
+        </Row>
+      </CSSTransitionGroup>
+    );
+
+    return (
+      <div ref="skillContainer" className="skill-container">
+        { this.state.showContainerItems ? animationContainer : <div></div> }
+      </div>
+    );
+  }
+}
 
 export default SkillContainer;
